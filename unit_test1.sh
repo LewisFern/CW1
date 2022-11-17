2dc717741861a5626eb2e1e0c29ef4849eda1f4f
LewisFern <lewisfern10@gmail.com>
2022-11-17 18:02:27 +0000

    tests

--- unit_test1.sh
+++ unit_test1.sh
@@ -4,7 +4,7 @@ java CurrencyConverter
 
 expected_value1 = ("1.0 Dollars = 0.74 Pounds1.0 Dollars = 0.88 EurosThank you for using the converter.")
 
-actual_value1 = java CurrencyConverter 1 dollars
+actual_value1 = (java CurrencyConverter 1 dollars)
 
 
 if [ "$expected_value1" = "$actual_value1" ]; then
