610ef9fbc2b27190678c7aad4bf1f8d050fac3a5
LewisFern <lewisfern10@gmail.com>
2022-11-17 12:07:33 +0000

    tests

---
+++ unit_test1.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+./CurrencyConverter.java
+
+echo "Please enter a value to convert"
+read input
+
+expected = "1 dollar" = "0.74 pounds" = "0.88 Euros"
+
+conversion = input * expected
+echo conversion
+
+
+
