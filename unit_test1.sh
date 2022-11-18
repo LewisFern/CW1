6001401edb993bc7112e19877d793e0216805f03
LewisFern <lewisfern10@gmail.com>
2022-11-18 14:45:21 +0000

    tests

--- unit_test1.sh
+++ unit_test1.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
-java CurrencyConverter
-
 #Test 1
-expected_value1=$'1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter.'
+expected_value1=$'1.0 Dollars = 0.74 Pounds
+1.0 Dollars = 0.88 Euros
+Thank you for using the converter.'
 actual_value1 = java CurrencyConverter 1 dollars
+
+echo "$expected_value1"
 
 if [ "$expected_value1" = "$actual_value1" ]; then
 echo "TEST PASS"
