3cdd2382a652101a76059705aa2e286c24529b58
LewisFern <lewisfern10@gmail.com>
2022-11-18 15:16:13 +0000

    tests

--- unit_test1.sh
+++ unit_test1.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#Test 1
+#Test 1 - Conversion test
 expected_value1=$'1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter.'
 actual_value1=`java CurrencyConverter 1 dollars`
 
@@ -13,7 +13,7 @@ echo "TEST FAIL"
 exit 1
 fi
 
-#Test 2
+#Test 2 - No input test
 expected_value2=$"1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter"
 actual_value2=`java CurrencyConverter `
 
@@ -23,6 +23,18 @@ else
 echo "TEST FAIL"
 exit 1
 fi
+
+#Test 3 - Incorrect format test
+expected_value2=$"1.0 Dollars = 0.74 Pounds\n1.0 Dollars = 0.88 Euros\nThank you for using the converter"
+actual_value2=`java CurrencyConverter dollar 1`
+
+if [ "$expected_value3" = "$actual_value3" ]; then
+echo "TEST PASS"
+else
+echo "TEST FAIL"
+exit 1
+fi
+
 
 
 
